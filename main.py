1fb328a922eb6be994cd4b3d2948f1ca68f77bc4
ViniOlive <olivervini7141@gmail.com>
2022-12-05 20:07:03 -0300

    Atualizacao script api

---
+++ main.py
@@ -0,0 +1,39 @@
+import json
+
+import pymongo
+from pymongo import MongoClient
+from flask import Flask, request, jsonify
+from datetime import datetime
+
+app = Flask(__name__)
+user_db = "Dev"
+pass_db = "teste123"
+db_name = "StockSale"
+
+
+def connect():
+    connection_string = f"mongodb+srv://{user_db}:{pass_db}@stockcontroldev.v8gl9k6.mongodb.net/?retryWrites=true"
+    client = MongoClient(connection_string)
+
+    return client[db_name]
+
+
+@app.route('/Product/List', methods=['GET'])
+def consultProduct():
+    db_table = connect()['Stock']
+    product_id = request.args.get('Product_ID')
+
+    dict_item_aux = {}
+    if product_id:
+        product = [item for item in db_table.find({"Product_ID": int(product_id)})]
+
+        for item in product:
+            for key, value in item.items():
+                if not key == '_id':
+                    dict_item_aux.update({key: value})
+
+        return jsonify(dict_item_aux)
+
+app.run(port=5000, host='localhost', debug=True)
+
+
