9981be3b7f9427694ca6b2888aefed2067c0f084
doliveira <voliveira@businessintegrator.com.br>
2022-12-09 17:12:00 -0300

    Alterações no jasvascript

--- Andrea Botasso - Calçados/assets/js/main.js
+++ Andrea Botasso - Calçados/assets/js/main.js
@@ -2,58 +2,54 @@ const formulario = document.querySelector('#Registro_vendas');
 const idProduto = document.querySelector('#id_produto');
 const valorProduto = document.querySelector('#valor');
 const quantidade = document.querySelector('#Quantidade');
-
-
-const url_API = "http://localhost:5000/Product/List?Product_ID"
-
+date = new Date().toISOString().slice(0, 10)
 const variavel_teste = document.getElementById('id_produto')
 
-formulario.addEventListener("submit", (e) => {  
-    
+const requestApi = async(productID) => {
+    const apiID = "http://localhost:5000/Product/List?Product_ID=".concat(productID);
+    const resultado = await fetch(apiID);
+    const data = resultado.json();
+    return data
+};
+
+idProduto.addEventListener("blur", e => {
     e.preventDefault();
     const idDoProduto = variavel_teste.value;
-
-    if (quantidade.value <=0){
-        alert('Informe uma quantidade valida!')
-
-
-    } else if(quantidade.value != Number(quantidade.value)){
-        alert('Informe uma quantidade valida!')
-    }  
-    else{
-
+    if (idProduto){
         try {
-            capturaValorProduto(idDoProduto);      
-        
+            capturaValorProduto(idDoProduto);
         } catch(err){
             window.alert("Produto não cadastrado!")
-        } 
+        }
+        }
+    else{
+        alert("Informe um número de produto valido.")
+    }
 
-    }       
-      
 });  
+
+quantidade.addEventListener("blur", e => {
+    e.preventDefault();
+     if (quantidade.value <=0){
+            alert('Informe uma quantidade valida!')
+        } else if(quantidade.value != Number(quantidade.value)){
+            alert('Informe uma quantidade valida!')
+        }
+        else{
+            document.getElementById('valor').value = `R$: ${quant * quantidade.value}`
+        }
+});
 //Realiza a captação de dados da API
-
-const capturaValorProduto = async(variavel_teste) => {  
-
+const capturaValorProduto = product => {
     try{
- 
-    const apiID = "http://localhost:5000/Product/List?Product_ID=".concat(variavel_teste);
+        const data = requestApi(product)
 
-    const resultado = await fetch(apiID);
-    const data = await resultado.json();
-    
-    console.log(data)
-    
-    console.log(data['description'])
-    const description = document.getElementById('descricao').value = data['description']
-    const valor = document.getElementById('valor').value = `R$: ${data['value'] * quantidade.value} `
-    
-    
-
+        console.log(data.)
+//        document.getElementById('descricao').value = data['description']
+//        document.getElementById('valor').value = `R$: ${data['value']}`
+//        document.getElementById('Data_venda').value = String(date)
     }catch(err){
         window.alert("Produto não cadastrado!")
     };
 };
-
 
